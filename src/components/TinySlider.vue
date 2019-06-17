6b56a8676abe36c87e6c405754202b1294c8fa4f
Katherine Aguilera <katherinam35@gmail.com>
2019-06-17 11:15:48 -0500

    Slider utilizando e integrando libreria tn-slider

---
+++ src/components/TinySlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="slider-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { tns } from 'tiny-slider/src/tiny-slider';
+import 'tiny-slider/dist/tiny-slider.css';
+
+export default {
+  name: 'TinySlider',
+  data() {
+    return {
+      // $ antes de una variable significa que no se aplicara reactividad al elemento
+      $slider: null,
+    };
+  },
+  mounted() {
+    // funcion de inicializacion
+    this.init();
+  },
+  methods: {
+    init() {
+      const settings = {
+        // donde se montara la libreria del slider (elemento padre)
+        container: this.$el,
+        mode: 'gallery',
+      };
+      this.$slider = tns(settings);
+    }
+  },
+  beforeDestroy() {
+    if (this.$slider) {
+      this.$slider.destroy();
+    }
+  },
+}
+</script>
+
