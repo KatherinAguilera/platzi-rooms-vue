9fd1a5d90d96bdccc2f5bb6fda6078497d36d643
Katherine Aguilera <katherinam35@gmail.com>
2019-06-20 16:55:25 -0500

    Obtencion de datos firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBd3WrOq-y6UMjdXO5gfj8-sAX077JMbEc',
+  authDomain: 'rooms-e3fb0.firebaseapp.com',
+  databaseURL: 'https://rooms-e3fb0.firebaseio.com',
+  projectId: 'rooms-e3fb0',
+  storageBucket: 'rooms-e3fb0.appspot.com',
+  messagingSenderId: '671700275024',
+  appId: '1:671700275024:web:2add61d02d78de0f',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import sourceData from './data.json';
+import firebase from 'firebase';
+// import sourceData from './data.json';
 import countObjectProperties from './utils';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    ...sourceData,
-    user: null,
+    // ...sourceData,
     authId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1',
     // Logica con vuex del modal
     modals: {
@@ -30,6 +30,14 @@ export default new Vuex.Store({
     APPEND_ROOM_TO_USER(state, { roomId, userId }) {
       Vue.set(state.users[userId].rooms, roomId, roomId);
     },
+    SET_ITEM(state, { item, id, resource }) {
+      const newItem = item;
+      newItem['.key'] = id;
+      Vue.set(state[resource], id, newItem);
+    },
+    SET_AUTHID(state, id) {
+      state.authId = id;
+    },
   },
   actions: {
     TOGGLE_MODAL_STATE: ({ commit }, { name, value }) => {
@@ -44,6 +52,27 @@ export default new Vuex.Store({
       commit('SET_ROOM', { newRoom, roomId });
       commit('APPEND_ROOM_TO_USER', { roomId, userId: newRoom.userId });
     },
+    FETCH_ROOMS: ({ state, commit }, limit) => new Promise((resolve) => {
+      let instance = firebase.database().ref('rooms');
+      if (limit) {
+        // los primeros que se encuentre en ese limite
+        instance = instance.limitToFirst(limit);
+      }
+      instance.once('value', (snapshot) => {
+        const rooms = snapshot.val();
+        Object.keys(rooms).forEach((roomId) => {
+          const room = rooms[roomId];
+          commit('SET_ITEM', { resource: 'rooms', id: roomId, item: room });
+        });
+        resolve(Object.values(state.rooms));
+      });
+    }),
+    FETCH_USER: ({ state, commit }, { id }) => new Promise((resolve) => {
+      firebase.database().ref('users').child(id).once('value', (snapshot) => {
+        commit('SET_ITEM', { resource: 'users', id: snapshot.key, item: snapshot.val() });
+        resolve(state.users[id]);
+      });
+    }),
   },
   getters: {
     // retornar obtener los datos del state
