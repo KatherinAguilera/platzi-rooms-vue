cca1295f8fad09cf66c81ce796990e77b3212c9e
Katherine Aguilera <katherinam35@gmail.com>
2019-06-21 16:38:41 -0500

    Agregando nuevos usuarios en firebase autentication

--- src/main.js
+++ src/main.js
@@ -19,6 +19,12 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch('FETCH_AUTH_USER');
+  }
+});
+
 new Vue({
   router,
   store,
